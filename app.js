2a88eb000099322f2a17b87b4a096b550f7dfe41
edgard <edgard@email.com>
2021-11-12 17:16:22 -0500

    Sexto commit

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("No le temas a él");
 alert("Spider-Man: No Way Home");
 document.write("No será lo mismo sin ti");
 document.write("Como un perro");
+
+document.write("Paramore - Monster.mp3");
