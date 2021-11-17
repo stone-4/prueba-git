dd87e025d78b1f6cc46ba3f560643f92b51b2bf8
edgard <edgard@email.com>
2021-11-17 12:44:30 -0500

    Código generador de conflicto en la rama2

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ if (grupo == "Panda") {
 
     document.write("Línea de código generado en la 'rama2'");
 
-    document.write("Segunda Línea de código generado en la 'rama2'");
+    alert("Código generado en la rama2, para generar conflicto");
 }
